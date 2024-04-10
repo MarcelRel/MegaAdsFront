ddf29b87505e79fcd52bcb8ce00a472fca0a75c4
MarcelRel <marcel.reliszka@wp.pl>
2024-04-10 20:07:33 +0200

    React router integrated and styles for button-like links fixed.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,8 @@
 import React, {SyntheticEvent, useContext, useState} from 'react';
-import {Btn} from "../common/Btn";
 
 import './Header.css';
 import {SearchContext} from "../../contexts/search.context";
+import {Btn} from "../common/Btn";
 
 export const Header = () => {
     const {search, setSearch} = useContext(SearchContext);
@@ -18,7 +18,7 @@ export const Header = () => {
             <h1>
                 <strong>Mega </strong> Ogłoszenia
             </h1>
-            <Btn text="Dodaj ogłoszenie"/>
+            <Btn to="/add" text="Dodaj ogłoszenie"/>
             <form className="search" onSubmit={setSearchFromLocalState}>
                 <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/> <Btn text="Szukaj"/>
             </form>
